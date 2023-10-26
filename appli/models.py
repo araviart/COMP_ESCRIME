5859be173620e6e78c7af4bcbf7df313fe847748
Nathan.Pigoreau <nathan.pigoreau@etu.univ-orleans.fr>
2023-10-26 09:37:26 +0200

    Optimisation : ajout des backrefs et des __tablename__ pour simplifier l'accès aux informations dans les relations de la base de données.

--- appli/models.py
+++ appli/models.py
@@ -2,6 +2,7 @@ from .app import db
 
 # Modèle pour représenter le lieu
 class Lieu(db.Model):
+    __tablename__ = 'LIEU'
     idLieu = db.Column(db.Integer, primary_key=True)
     nomLieu = db.Column(db.String(50), nullable=False)
     villeLieu = db.Column(db.String(50), nullable=False)
@@ -10,11 +11,13 @@ class Lieu(db.Model):
 
 # Modèle pour représenter l'arme
 class Arme(db.Model):
+    __tablename__ = 'ARME'
     idArme = db.Column(db.Integer, primary_key=True)
     nomArme = db.Column(db.String(50), nullable=False)
 
 # Modèle pour représenter la saison
 class Saison(db.Model):
+    __tablename__ = 'SAISON'
     idSaison = db.Column(db.Integer, primary_key=True)
     nomSaison = db.Column(db.String(50), nullable=False)
     dateDebutSaison = db.Column(db.Date, nullable=False)
@@ -22,17 +25,20 @@ class Saison(db.Model):
 
 # Modèle pour représenter la catégorie
 class Categorie(db.Model):
+    __tablename__ = 'CATEGORIE'
     idCat = db.Column(db.Integer, primary_key=True)
     nomCategorie = db.Column(db.String(50), nullable=False)
 
 # Modèle pour représenter le club
 class Club(db.Model):
+    __tablename__ = 'CLUB'
     idClub = db.Column(db.Integer, primary_key=True)
     nomClub = db.Column(db.String(50), nullable=False)
     villeClub = db.Column(db.String(50), nullable=False)
 
 # Modèle pour représenter la compétition
 class Competition(db.Model):
+    __tablename__ = 'COMPETITION'
     idComp = db.Column(db.Integer, primary_key=True)
     idLieu = db.Column(db.Integer, db.ForeignKey('lieu.idLieu'), nullable=False)
     idSaison = db.Column(db.Integer, db.ForeignKey('saison.idSaison'), nullable=False)
@@ -45,28 +51,31 @@ class Competition(db.Model):
     sexeComp = db.Column(db.String(1), nullable=False)
     estIndividuelle = db.Column(db.Boolean, nullable=False)
 
-    lieu = db.relationship('Lieu')
-    saison = db.relationship('Saison')
-    categorie = db.relationship('Categorie')
-    arme = db.relationship('Arme')
+    lieu = db.relationship('Lieu', backref='Lieu.idLieu')
+    saison = db.relationship('Saison', backref='Saison.idSaison')
+    categorie = db.relationship('Categorie', backref='Categorie.idCat')
+    arme = db.relationship('Arme', backref='Arme.idArme')
 
 # Modèle pour représenter la piste
 class Piste(db.Model):
+    __tablename__ = 'PISTE'
     idPiste = db.Column(db.Integer, primary_key=True)
     idComp = db.Column(db.Integer, db.ForeignKey('competition.idComp'), nullable=False)
     nomPiste = db.Column(db.String(50), nullable=False)
     estDispo = db.Column(db.Boolean, nullable=False)
 
-    competition = db.relationship('Competition')
+    competition = db.relationship('Competition', backref='Competition.idComp')
 
 # Modèle pour représenter le type de match
 class TypeMatch(db.Model):
+    __tablename__ = 'TYPE_MATCH'
     idTypeMatch = db.Column(db.Integer, primary_key=True)
     nomTypeMatch = db.Column(db.String(50), nullable=False)
     nbTouches = db.Column(db.Integer, nullable=False)
 
 # Modèle pour représenter l'escrimeur
 class Escrimeur(db.Model):
+    __tablename__ = 'ESCRIMEUR'
     idEscrimeur = db.Column(db.Integer, primary_key=True)
     idCat = db.Column(db.Integer, db.ForeignKey('categorie.idCat'), nullable=False)
     prenomE = db.Column(db.String(50), nullable=False)
@@ -76,60 +85,67 @@ class Escrimeur(db.Model):
     sexeE = db.Column(db.String(50), nullable=False)
     numTelE = db.Column(db.Integer)
 
-    categorie = db.relationship('Categorie')
+    categorie = db.relationship('Categorie', backref='Categorie.idCat')
 
 # Modèle pour représenter les tireurs
 class Tireur(db.Model):
+    __tablename__ = 'TIREUR'
     idTireur = db.Column(db.Integer, primary_key=True)
     idClub = db.Column(db.Integer, db.ForeignKey('club.idClub'), nullable=False)
     classement = db.Column(db.Integer, nullable=False)
 
-    club = db.relationship('Club')
+    club = db.relationship('Club', backref='Club.idClub')
 
 # Modèle pour représenter les arbitres
 class Arbitre(db.Model):
+    __tablename__ = 'ARBITRE'
     idArbitre = db.Column(db.Integer, primary_key=True)
 
 # Modèle pour représenter la relation entre les escrimeurs et les armes qu'ils pratiquent
 class PratiquerArme(db.Model):
+    __tablename__ = 'PRATIQUER_ARME'
     idEscrimeur = db.Column(db.Integer, db.ForeignKey('escrimeur.idEscrimeur'), primary_key=True)
     idArme = db.Column(db.Integer, db.ForeignKey('arme.idArme'), primary_key=True)
 
-    escrimeur = db.relationship('Escrimeur')
-    arme = db.relationship('Arme')
+    escrimeur = db.relationship('Escrimeur', backref='Escrimeur.idEscrimeur')
+    arme = db.relationship('Arme', backref='Arme.idArme')
 
 # Modèle pour représenter le classement final
 class ClassementFinal(db.Model):
+    __tablename__ = 'CLASSEMENT_FINAL'
     idClassementFinal = db.Column(db.Integer, primary_key=True)
     idComp = db.Column(db.Integer, db.ForeignKey('competition.idComp'), nullable=False)
     idTireur = db.Column(db.Integer, db.ForeignKey('tireur.idTireur'), nullable=False)
     position = db.Column(db.Integer, nullable=False)
 
-    competition = db.relationship('Competition')
-    tireur = db.relationship('Tireur')
+    competition = db.relationship('Competition', backref='Competition.idComp')
+    tireur = db.relationship('Tireur', backref='Tireur.idTireur')
 
 # Modèle pour représenter les poules
 class Poule(db.Model):
+    __tablename__ = 'POULE'
     idPoule = db.Column(db.Integer, primary_key=True)
     idComp = db.Column(db.Integer, db.ForeignKey('competition.idComp'), nullable=False)
     idPiste = db.Column(db.Integer, db.ForeignKey('piste.idPiste'), nullable=False)
     idArbitre = db.Column(db.Integer, db.ForeignKey('arbitre.idArbitre'), nullable=False)
     nomPoule = db.Column(db.String(50), nullable=False)
 
-    competition = db.relationship('Competition')
-    piste = db.relationship('Piste')
-    arbitre = db.relationship('Arbitre')
+    competition = db.relationship('Competition', backref='Competition.idComp')
+    piste = db.relationship('Piste', backref='Piste.idPiste')
+    arbitre = db.relationship('Arbitre', backref='Arbitre.idArbitre')
 
 # Modèle pour représenter les participants aux poules
 class ParticipantsPoule(db.Model):
+    __tablename__ = 'PARTICIPANTS_POULE'
     idPoule = db.Column(db.Integer, db.ForeignKey('poule.idPoule'), primary_key=True)
     idTireur = db.Column(db.Integer, db.ForeignKey('tireur.idTireur'), primary_key=True)
 
-    poule = db.relationship('Poule')
-    tireur = db.relationship('Tireur')
+    poule = db.relationship('Poule', backref='Poule.idPoule')
+    tireur = db.relationship('Tireur', backref='Tireur.idTireur')
 
 # Modèle pour représenter les matchs de poule
 class MatchPoule(db.Model):
+    __tablename__ = 'MATCH_POULE'
     idMatch = db.Column(db.Integer, primary_key=True)
     idTypeMatch = db.Column(db.Integer, db.ForeignKey('type_match.idTypeMatch'), nullable=False)
     idPoule = db.Column(db.Integer, db.ForeignKey('poule.idPoule'), nullable=False)
@@ -144,15 +160,16 @@ class MatchPoule(db.Model):
     touchesRecuesTireur2 = db.Column(db.Integer)
     touchesDonneesTireur2 = db.Column(db.Integer)
 
-    type_match = db.relationship('TypeMatch')
-    poule = db.relationship('Poule')
-    piste = db.relationship('Piste')
-    arbitre = db.relationship('Arbitre')
-    tireur1 = db.relationship('Tireur', foreign_keys=[idTireur1])
-    tireur2 = db.relationship('Tireur', foreign_keys=[idTireur2])
+    type_match = db.relationship('TypeMatch', backref='TypeMatch.idTypeMatch')
+    poule = db.relationship('Poule', backref='Poule.idPoule')
+    piste = db.relationship('Piste', backref='Piste.idPiste')
+    arbitre = db.relationship('Arbitre', backref='Arbitre.idArbitre')
+    tireur1 = db.relationship('Tireur', foreign_keys=[idTireur1], backref='Tireur.idTireur1')
+    tireur2 = db.relationship('Tireur', foreign_keys=[idTireur2], backref='Tireur.idTireur2')
 
 # Modèle pour représenter les feuilles de match
 class FeuilleMatch(db.Model):
+    __tablename__ = 'FEUILLE_MATCH'
     idFeuille = db.Column(db.Integer, primary_key=True)
     idPoule = db.Column(db.Integer, db.ForeignKey('poule.idPoule'), nullable=False)
     idComp = db.Column(db.Integer, db.ForeignKey('competition.idComp'), nullable=False)
@@ -161,7 +178,7 @@ class FeuilleMatch(db.Model):
     scoreTireur1 = db.Column(db.Integer)
     scoreTireur2 = db.Column(db.Integer)
 
-    poule = db.relationship('Poule')
-    competition = db.relationship('Competition')
-    tireur1 = db.relationship('Tireur', foreign_keys=[idTireur1])
-    tireur2 = db.relationship('Tireur', foreign_keys=[idTireur2])
+    poule = db.relationship('Poule', backref='Poule.idPoule')
+    competition = db.relationship('Competition', backref='Competition.idComp')
+    tireur1 = db.relationship('Tireur', foreign_keys=[idTireur1], backref='Tireur.idTireur1')
+    tireur2 = db.relationship('Tireur', foreign_keys=[idTireur2], backref='Tireur.idTireur2')
