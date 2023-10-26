35ceef8332b43d369bf15a21b9e9e34f8904e07e
Irvyn COURSIMAULT <irvyncsm@gmail.com>
2023-10-26 14:43:16 +0200

    ajout bootstrap et login

--- appli/app.py
+++ appli/app.py
@@ -1,6 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap5
+from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:irvyn@localhost/COUP_ESCRIME'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:irvyn@localhost/COMP_ESCRIME'
 db = SQLAlchemy(app)
+
+app. config['BOOTSTRAP_SERVE_LOCAL'] = True
+bootstrap = Bootstrap5(app)
+
+login_manager = LoginManager(app)
+
+app.config["SECRET_KEY"] = "2219f8a3-ca94-4312-a411-cb3d051a1238"
