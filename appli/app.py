335c551b2ffa0f93a09c221d44d3f58844a977b7
krshmt <122864621+krshmt@users.noreply.github.com>
2023-11-06 22:33:24 +0100

    ajout du contenu de la page

--- appli/app.py
+++ appli/app.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap5
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://coursimault:coursimault@servinfo-maria/DBcoursimault'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:toure@localhost/COUP_ESCRIME'
 db = SQLAlchemy(app)
 
-app. config['BOOTSTRAP_SERVE_LOCAL'] = True
-bootstrap = Bootstrap5(app)
+app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+bootstrap = Bootstrap(app)
 
 login_manager = LoginManager(app)
 
 app.config["SECRET_KEY"] = "2219f8a3-ca94-4312-a411-cb3d051a1238"
+
