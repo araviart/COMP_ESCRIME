0bd604118f0dc357cdff233e5d3ffbdfa9ec4043
krshmt <tourekris16@gmail.com>
2023-11-07 15:11:13 +0100

    suppression de code du a une dupplication

--- appli/views.py
+++ appli/views.py
@@ -21,50 +21,6 @@ class LoginForm(FlaskForm):
 @app.route("/")
 def home():
     return render_template(
-    "ajout-comp.html",
-    title="Compétions ESCRIME",
-    competitions=get_sample())
-
-@app.route("/login/", methods =("GET","POST",))
-def login():
-    f = LoginForm()
-    if f.validate_on_submit():
-        user = f.get_authenticated_user()
-        if user:
-            login_user(user)
-            return redirect(url_for("home"))
-    return render_template(
-        "login.html",
-        form=f)
-
-@app.route("/logout/")
-def logout ():
-    logout_user ()
-    return redirect(url_for("home"))
-=======
-from .app import app
-from flask import render_template, url_for, redirect, request
-from .models import User, get_sample
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from hashlib import sha256
-from flask_login import login_user, logout_user
-
-class LoginForm(FlaskForm):
-    username = StringField ("Username")
-    password = PasswordField("Password")
-    def get_authenticated_user (self ):
-        user = User.query.get(self.username.data)
-        if user is None:
-            return None
-        m = sha256 ()
-        m.update(self.password.data.encode ())
-        passwd = m. hexdigest ()
-        return user if passwd == user.password else None
-    
-@app.route("/")
-def home():
-    return render_template(
     "competitionsBS.html",
     title="Compétions ESCRIME",
     competitions=get_sample())
@@ -91,4 +47,3 @@ def test_popup():
     return render_template(
         "test_popup.html",
         title="Test")
-
