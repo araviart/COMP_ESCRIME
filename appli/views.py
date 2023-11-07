76213b308b2b2b5ea331ed6b95f09b1eb5d2a3e2
Coursimault Irvyn <o22206139@info23-27.iut45.univ-orleans.fr>
2023-11-07 15:24:25 +0100

    redirection login fonctionnel sans test mdp

--- appli/views.py
+++ appli/views.py
@@ -1,46 +1,13 @@
 from .app import app
-from flask import render_template, url_for, redirect, request
-from .models import User, get_sample
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from hashlib import sha256
-from flask_login import login_user, logout_user
+from flask import render_template
 
-class LoginForm(FlaskForm):
-    username = StringField ("Username")
-    password = PasswordField("Password")
-    def get_authenticated_user (self ):
-        user = User.query.get(self.username.data)
-        if user is None:
-            return None
-        m = sha256 ()
-        m.update(self.password.data.encode ())
-        passwd = m. hexdigest ()
-        return user if passwd == user.password else None
-    
 @app.route("/")
 def home():
-    return render_template(
-    "competitionsBS.html",
-    title="Compétions ESCRIME",
-    competitions=get_sample())
+    return render_template("Login.html")
 
-@app.route("/login/", methods =("GET","POST",))
-def login():
-    f = LoginForm()
-    if f.validate_on_submit():
-        user = f.get_authenticated_user()
-        if user:
-            login_user(user)
-            return redirect(url_for("home"))
-    return render_template(
-        "login.html",
-        form=f)
-
-@app.route("/logout/")
-def logout ():
-    logout_user ()
-    return redirect(url_for("home"))
+@app.route("/ajout-comp", methods=['POST'])
+def ajout_comp_page():
+    return render_template("ajout-comp.html")
 
 @app.route("/test_popup/")
 def test_popup():
