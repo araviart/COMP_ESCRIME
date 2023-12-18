4d3a51e3bf10562de5c1720560ecf640cd318833
krshmt <tourekris16@gmail.com>
2023-12-18 11:57:38 +0100

    ajout du bouton pour valider une poule et suppression import migrate

--- appli/app.py
+++ appli/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap4
 from flask_login import LoginManager
-from flask_migrate import Migrate
+#from flask_migrate import Migrate
 
 # username = 'root'
 # password = 'root'
