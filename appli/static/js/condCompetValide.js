beca0c05a4516eb216f96666303a976dc284c519
araviart <alexandre.raviart@etu.univ-orleans.fr>
2023-12-20 00:30:42 +0100

    fix redirection

--- appli/static/js/condCompetValide.js
+++ appli/static/js/condCompetValide.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     showError(errorMessage);
   } else {
-    window.location.href = "http://127.0.0.1:5000/gestion_poules/" + competitionId;
+    window.location.href = "/gestion_poules/" + competitionId;
   }
   
   function showError(message) {
